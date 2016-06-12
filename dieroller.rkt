21f86f14dd0fcdd2eda5f6f36914135d64152e64
H. Dale McBane <h.dale.mcbane@gmail.com>
2016-06-12 19:27:33 -0400

    Improved use of named parameters
    - added dice parameter
    - added sides parameter
    - added modifier parameter

--- dieroller.rkt
+++ dieroller.rkt
@@ -12,39 +12,77 @@
 
 
 (define DIEROLLERHELP "Try 'dieroller --help' for more information.")(define number-of-rolls-to-keep (make-parameter null))
+(define number-to-roll (make-parameter 1))
+(define sides-per-die (make-parameter 20))
 (define number-to-keep (make-parameter 0))
+(define modifier-to-rolls (make-parameter "0"))
 
 ;; A dice-rolling command-line utility
 (command-line
  ;; remove the following comment to test from DrRacket
- ;; #:argv (list "6" "6" "+3")
+ ;; #:argv (list "1" "10")
+ ;; #:argv (list "3" "6" "+3")
+ ;; #:argv (list "--dice" "5" "--sides" "100" "--modifier" "+4" "--keep" "3")
+ ;; #:argv (list "--dice" "4" "--sides" "6" "--keep" "3")
+ ;; #:argv (list "--help")
  #:usage-help
  ""
- "where the <arguments> are [dice] [sides] and [modifier]"
- "  [dice] must be a number greater than 0."
- "  [sides] must be a number greater than 0."
- "  [modifier] is optional.  It can be an optional +, -, or * followed by a number." 
- "    If the +, -, or * is missing, + is assumed."
+ "where the <arguments> are"
+ ""
+ "  <dice> <sides>"
+ "or"
+ "  <dice> <sides> <modifier>" 
+ ""
+ "See the --dice, --sides, and --modifier parameters for details."
  ""
  "Examples:"
  ""
  "  dieroller 1 10"
- "  dieroller 3 6 +6"
- "  dieroller 2 100 -4"
+ "  dieroller 3 6 +3"
+ "  dieroller --dice 5 --sides 100 --modifier +4 --keep 3"
+ "  dieroller --dice 4 --sides 6 --keep 3"
  ""
  #:once-each
- [("-k" "--keep") number
-                  ("Number of rolls to keep"
+ [("-d" "--dice") dice ("Number of dice to roll.  Must be greater than 0."
+                        "(default to 1)")
+                  (number-to-roll (string->number dice))]
+ [("-k" "--keep") keep
+                  ("Number of rolls to keep. Must be greater than 0 and less than or equal to <dice>."
                    "(default to all)")
-                  (number-to-keep (string->number number))]
+                  (number-to-keep (string->number keep))]
+ [("-m" "--modifier") modifier
+                      ("Modifier to the rolls. The first character can optionally"
+                       "be one of +, -, or * followed by a number.  If the +, -, or"
+                       "* are missing, + is assumed. (default to no modifier)")
+                      (modifier-to-rolls modifier)]
+ [("-s" "--sides") sides
+                   ("Number of sides per die. Must be greater than 0."
+                    "(default to 20)")
+                   (sides-per-die (string->number sides))]
  #:args arguments
- (let* ([dice (if (< (length arguments) 1) 1 (string->number (first arguments)))]
-        [keep (if (< (number-to-keep) 1) dice (number-to-keep))]
-        [sides (if (< (length arguments) 2) 20 (string->number (second arguments)))]
-        [modifier (parse-modifier (if (< (length arguments) 3) "0" (third arguments)))]
+ (let* ([dice (if (< (length arguments) 1)
+                  (number-to-roll)
+                  (string->number (first arguments)))]
+        [keep (if (< (number-to-keep) 1)
+                  dice
+                  (number-to-keep))]
+        [sides (if (< (length arguments) 2)
+                   (sides-per-die)
+                   (string->number (second arguments)))]
+        [modifier (parse-modifier (if (< (length arguments) 3)
+                                      (modifier-to-rolls)
+                                      (third arguments)))]
         [op (first modifier)]
         [amt (second modifier)]
-        [dicetype (string-append (number->string dice) "D" (number->string sides) " " (third modifier) " keep " (number->string keep))]
+        [dicetype (string-append (number->string dice)
+                                 "D"
+                                 (number->string sides)
+                                 (if (equal? (third modifier) "+0")
+                                     ""
+                                     (third modifier))
+                                 (if (= dice keep)
+                                     ""
+                                     (string-append " keep " (number->string keep))))]
         [myrand (lambda (x) (+ 1 (random sides)))])
    (cond [(< dice 1) (begin
                        (displayln "dice must be greater than 0.")
@@ -57,9 +95,14 @@
                       [sum (apply + maxkeep)]
                       [adjusted (op sum amt)])
                  (displayln dicetype)
-                 (display "Result: (")
-                 (display (string-join (map number->string rands) " "))
-                 (display ") => ")
+                 (display "Result: ")
+                 (if (> (length rands) 1)
+                     (begin
+                       (display "(")
+                       (display (string-join (map number->string rands) " "))
+                       (display ") "))
+                     (null? null))
+                 (display "=> ")
                  (displayln adjusted)
                  )])
    ))
